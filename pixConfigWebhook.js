0b6596bed35ecc99a4cc6d545550e9c35b65a126
Lucas Marçal <lucasmarcal@gmail.com>
2023-05-24 16:20:43 +0300

    fix webhook config function.

--- pixConfigWebhook.js
+++ pixConfigWebhook.js
@@ -3,22 +3,24 @@ let options = require('./credentials')
 
 options['validateMtls'] = false
 
-let body = {
-	webhookUrl: 'https://api-pagamentos.pimentamarshall.com.br/webhook',
-}
-
-let params = {
-	chave: 'f6518e9e-a21b-426f-9596-b000c7dbf9ab',
-}
-
 const gerencianet = new Gerencianet(options)
 
-const pixConfigWebhook = gerencianet.pixConfigWebhook(params, body)
+const pixConfigWebhook = async () => {
+    let body = {
+        webhookUrl: 'https://api-pagamentos.pimentamarshall.com.br/webhook',
+    }
+    
+    let params = {
+        chave: 'f6518e9e-a21b-426f-9596-b000c7dbf9ab',
+    }
+    
+    gerencianet.pixConfigWebhook(params, body)
 	.then((resposta) => {
 		return resposta
 	})
 	.catch((error) => {
 		console.log(error)
 	})
+} 
 
 module.exports = pixConfigWebhook
