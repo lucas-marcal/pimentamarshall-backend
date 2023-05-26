5d5c4d2e2f9ce796439208d3cd003bc5bc1e515c
Lucas Marçal <lucasmarcal@gmail.com>
2023-05-26 17:15:01 +0300

    Add prisma. add function to update payment status.

--- app.js
+++ app.js
@@ -1,27 +1,45 @@
-const express = require("express")
-const cors = require("cors")
-const { createNewPix } = require("./pixCreateImmediateCharge")
+const express = require("express");
+const cors = require("cors");
+const { createNewPix } = require("./pixCreateImmediateCharge");
+const { pixUpdateOrderStatus } = require("./prismaFunctions");
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send({ ok: true })
-})
-app.post("/create-order", async(req, res) => {
-    const {qrcode, imagemQrcode} = await createNewPix(req.body)
-    res.send({ ok: 1, qrcode, imagemQrcode })
-})
+  res.send({ ok: true });
+});
+app.post("/create-order", async (req, res) => {
+  const { qrcode, imagemQrcode } = await createNewPix(req.body);
+  res.send({ ok: 1, qrcode, imagemQrcode });
+});
 
-app.post("/webhook*", (req, res) => {
-    console.log("webhook received.")
-    console.log(req.body)
-    if (!req.client.authorized){
-        return res.status(401).send("Invalid client certificate.")
-    }
-    res.send({ ok: 1 })
-})
+app.post("/webhook*", async (req, res) => {
+  console.log("webhook received.");
+  const { pix } = req.body;
+  if (!req.client.authorized) {
+    return res.status(401).send("Invalid client certificate.");
+  }
 
-module.exports = app
+  if (pix) {
+    console.log(pix)
+    await pixUpdateOrderStatus(pix[0].txid)
+  }
+  res.send({ ok: 1 });
+});
+
+// {
+//   pix: [
+//     {
+//       endToEndId: "E00416968202305241352lsYWO1u5oPw",
+//       txid: "e571c7ed915d45de92a35257f8b48c13",
+//       chave: "f6518e9e-a21b-426f-9596-b000c7dbf9ab",
+//       valor: "0.03",
+//       horario: "2023-05-24T13:52:36.000Z",
+//     },
+//   ];
+// }
+
+module.exports = app;
---
+++ prismaFunctions.js
@@ -0,0 +1,26 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+async function pixUpdateOrderStatus(txid) {
+  try {
+    await prisma.shopOrder
+      .update({
+        where: { txid: txid },
+        data: { status: "PROCESSING" },
+      })
+      .then(async () => {
+        await prisma.$disconnect();
+      });
+  } catch (error) {
+    console.error(error);
+
+    await prisma.$disconnect();
+
+    process.exit(1);
+  }
+}
+
+module.exports = {
+    pixUpdateOrderStatus,
+}
