a1dc3d23d93e4d1784901a8f5ba037223107c1aa
Lucas Marçal <lucasmarcal@gmail.com>
2023-06-06 18:57:03 +0300

    Add name do sender email.

--- sendOrderReceipt.js
+++ sendOrderReceipt.js
@@ -8,23 +8,24 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 async function sendPixConfirmation(clientEmail) {
   const msg = {
     to: clientEmail,
-    from: "contato@pimentamarshall.com.br",
+    from: {
+      email: "contato@pimentamarshall.com.br",
+      name: "Pimenta Marshall",
+    },
     templateId: "d-6a99e27c1fd24fed878772c8c49d8998",
     dynamicTemplateData: {},
   };
 
   sgMail
-  .send(msg)
-  .then(() => {
-    console.log("Email sent");
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+    .send(msg)
+    .then(() => {
+      console.log("Email sent");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
 
 module.exports = {
   sendPixConfirmation,
-}
-
-
+};
