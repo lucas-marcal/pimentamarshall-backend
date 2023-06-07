623a4aebfb27580264af0df6e8aa0211bfaf1d46
Lucas Marçal <lucasmarcal@gmail.com>
2023-06-07 20:54:02 +0300

    testing charges update.

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.post("/create-one-step-link", async (req, res) => {
 
 app.post("/recebimento", async (req, res) => {
   console.log("Card or Billet update received.");
-  console.log(req);
+  console.log(req.body);
   res.send({ ok: 1 });
 });
 
