e23e39e75616d19077ea11a83bf5d815410bb173
Lucas Marçal <lucasmarcal@gmail.com>
2023-06-07 20:53:28 +0300

    testing charge updates.

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended: true, limit: '1mb'}))
 
 app.get("/", (req, res) => {
   res.send({ ok: true });
