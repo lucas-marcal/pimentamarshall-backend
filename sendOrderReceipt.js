e1f48ff225320ae6baceaf77fbf45bc81cddffc5
lucas-marcal <lucasmarcal@gmail.com>
2023-08-07 17:54:55 -0300

    Sending order confirmation to store's email.

--- sendOrderReceipt.js
+++ sendOrderReceipt.js
@@ -6,20 +6,39 @@ const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 async function sendPixConfirmation(clientEmail) {
-  const msg = {
+  const msg_client = {
     to: clientEmail,
     from: {
-      email: "contato@pimentamarshall.com.br",
+      email: "vendas@pimentamarshall.com.br",
       name: "Pimenta Marshall",
     },
     templateId: "d-6a99e27c1fd24fed878772c8c49d8998",
     dynamicTemplateData: {},
   };
 
+  const msg_store = {
+    to: "contato@pimentamarshall.com.br",
+    from: {
+      email: "vendas@pimentamarshall.com.br",
+      name: "Vendas Marshall",
+    },
+    templateId: "d-acce61474fd94d6e8edfd5edcf4e06dd",
+    dynamicTemplateData: {},
+  };
+
   sgMail
-    .send(msg)
+    .send(msg_client)
     .then(() => {
-      console.log("Email sent");
+      console.log("Email sent to client");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+  sgMail
+    .send(msg_store)
+    .then(() => {
+      console.log("Email sent to store.");
     })
     .catch((error) => {
       console.error(error);
