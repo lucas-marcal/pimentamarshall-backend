5c9a377181f658d0d05b44ebb9eb8d069ae2e0b4
Lucas Marçal <lucasmarcal@gmail.com>
2024-05-21 14:40:16 -0300

    Fix webhook

--- pixConfigWebhook.js
+++ pixConfigWebhook.js
@@ -1,26 +1,27 @@
-const Gerencianet = require('gn-api-sdk-node')
-let options = require('./credentials')
+const Gerencianet = require("gn-api-sdk-node");
+let options = require("./credentials");
 
-options['validateMtls'] = false
+options["validateMtls"] = true;
 
-const gerencianet = new Gerencianet(options)
+const gerencianet = new Gerencianet(options);
 
 const pixConfigWebhook = async () => {
-    let body = {
-        webhookUrl: 'https://api-pagamentos.pimentamarshall.com.br/webhook',
-    }
-    
-    let params = {
-        chave: 'f6518e9e-a21b-426f-9596-b000c7dbf9ab',
-    }
-    
-    gerencianet.pixConfigWebhook(params, body)
-	.then((resposta) => {
-		return resposta
-	})
-	.catch((error) => {
-		console.log(error)
-	})
-} 
+  let body = {
+    webhookUrl: "https://api-pagamentos.pimentamarshall.com.br/webhook",
+  };
 
-module.exports = pixConfigWebhook
+  let params = {
+    chave: "f6518e9e-a21b-426f-9596-b000c7dbf9ab",
+  };
+
+  gerencianet
+    .pixConfigWebhook(params, body)
+    .then((resposta) => {
+      return resposta;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+module.exports = pixConfigWebhook;
