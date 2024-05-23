ff78c2f96fbfbf6ef0cd54940b82d88f97e48d19
Lucas Marçal <lucasmarcal@gmail.com>
2024-05-23 16:00:04 -0300

    db connection troubleshooting

---
+++ dbtest.js
@@ -0,0 +1,20 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+async function getAllProducts() {
+  try {
+    const products = await prisma.product.findMany().then(async () => {
+      await prisma.$disconnect();
+    });
+    return products;
+  } catch (error) {
+    console.error(error);
+
+    await prisma.$disconnect();
+
+    process.exit(1);
+  }
+}
+
+console.log(getAllProducts());
